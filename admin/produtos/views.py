8b4fb87fbfed45c32e261f7d8e39b0ff7b2ae745
Marcelo Ceolin <mceolin@unidavi.edu.br>
2021-03-16 20:09:54 -0300

    inicializados metodos http do microservice backend

--- admin/produtos/views.py
+++ admin/produtos/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+
+class ProdutoViewSet(viewsets.ViewSet):
+    def get_all(self, request):
+        pass
+
+    def create(self, request):
+        pass
+
+    def get(self, request, pk=None):
+        pass
+
+    def update(self, request, pk=None):
+        pass
+
+    def delete(self, request, pk=None):
+        pass
