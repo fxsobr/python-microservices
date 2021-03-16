66aa61fa26180ab43b25ab629fd9a6b3fd17e451
Marcelo Ceolin <mceolin@unidavi.edu.br>
2021-03-16 20:24:19 -0300

    criado endpoint que cria um novo produto do microservice admin

--- admin/produtos/views.py
+++ admin/produtos/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.response import Response
 
 from .models import Produto
@@ -12,7 +12,10 @@ class ProdutoViewSet(viewsets.ViewSet):
         return Response(serializer.data)
 
     def create(self, request):
-        pass
+        serializer = ProdutoSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def retrieve(self, request, pk=None):
         pass
