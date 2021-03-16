5077efc5e6f19f3492f509875cc7b41a1adde31e
Marcelo Ceolin <mceolin@unidavi.edu.br>
2021-03-16 20:55:20 -0300

    criado models microservico main e criado comandos para migracao do db

--- main/main.py
+++ main/main.py
@@ -1,6 +1,29 @@
 from flask import Flask
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import UniqueConstraint
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@db/main'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+CORS(app)
+
+db = SQLAlchemy(app)
+
+
+class Produto(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=False)
+    titulo = db.Column(db.String(200))
+    image = db.Column(db.String(200))
+
+
+class ProdutoUsuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    usuario_id = db.Column(db.Integer)
+    produto_id = db.Column(db.Integer)
+
+    UniqueConstraint('usuario_id', 'produto_id', name='usuario_produto_unique')
 
 
 @app.route('/')
