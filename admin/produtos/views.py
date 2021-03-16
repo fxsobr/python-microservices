edca5bb789651db3e733ee4070dde2b74e277e46
Marcelo Ceolin <mceolin@unidavi.edu.br>
2021-03-16 20:31:20 -0300

    criado endpoint que lista um produto por id, atualiza um produto e remove um produto do microservice admin

--- admin/produtos/views.py
+++ admin/produtos/views.py
@@ -18,10 +18,18 @@ class ProdutoViewSet(viewsets.ViewSet):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def retrieve(self, request, pk=None):
-        pass
+        produto = Produto.objects.get(id=pk)
+        serializer = ProdutoSerializer(produto)
+        return Response(serializer.data)
 
     def update(self, request, pk=None):
-        pass
+        produto = Produto.objects.get(id=pk)
+        serializer = ProdutoSerializer(instance=produto, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
 
     def delete(self, request, pk=None):
-        pass
+        produto = Produto.objects.get(id=pk)
+        produto.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
