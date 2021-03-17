e57a08b6b7e7c2b05c13bd8c0da10402983f342a
Marcelo Ceolin <mceolin@unidavi.edu.br>
2021-03-16 21:35:15 -0300

    ajustes no consumer dos microservicos main(flask) e admin(django)

--- admin/consumer.py
+++ admin/consumer.py
@@ -14,7 +14,7 @@ def callback(channel, method, properties, body):
     print(body)
 
 
-channel.basic_consume(queue='admin', on_message_callback=callback)
+channel.basic_consume(queue='admin', on_message_callback=callback, auto_ack=True)
 
 print('Iniciando o consumer')
 
