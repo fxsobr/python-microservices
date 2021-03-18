0f34c12db7eb896152536b95c9a75ada2dae0a85
Marcelo Ceolin <mceolin@unidavi.edu.br>
2021-03-17 21:16:05 -0300

    criado metodos no consumer do microservice main que buscam dados do eventbus que são enviados pelo microservice admin

--- main/consumer.py
+++ main/consumer.py
@@ -1,4 +1,8 @@
+import json
+
 import pika
+
+from main import Produto, db
 
 params = pika.URLParameters('amqps://zfpolbnp:T-ZXlWGfp0YxqHyBUYDe11aTZlbwe1Lt@jackal.rmq.cloudamqp.com/zfpolbnp')
 
@@ -11,7 +15,24 @@ channel.queue_declare(queue='main')
 
 def callback(channel, method, properties, body):
     print('Recebendo no microservico main')
-    print(body)
+    data = json.loads(body)
+    print(data)
+
+    if properties.content_type == 'produto_criado':
+        produto = Produto(id=data['id'], titulo=data['titulo'], image=data['imagem'])
+        db.session.add(produto)
+        db.session.commit()
+
+    elif properties.content_type == 'produto_atualizado':
+        produto = Produto.query.get(data['id'])
+        produto.titulo = data['titulo']
+        produto.image = data['imagem']
+        db.session.commit()
+
+    elif properties.content_type == 'produto_removido':
+        produto = Produto.query.get(data)
+        db.session.delete(produto)
+        db.session.commit()
 
 
 channel.basic_consume(queue='main', on_message_callback=callback, auto_ack=True)
